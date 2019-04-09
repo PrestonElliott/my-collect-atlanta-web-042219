73514cf6abbac1d338ae0463e04dae0a238434c7
Preston Elliott <preston.elliott24@gmail.com>
2019-04-09 16:57:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,11 +8,12 @@ def my_collect(array)
   new_array
 end
 
-def my_each(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-  array
-end
+# my each example
+# def my_each(array)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+#   array
+# end
